c9e505c74234d4221c580e18e7acb861d0c61012
yasmeen abdallah <yasmeenabdallah11898@gmail.com>
2020-11-26 20:59:22 +0200

    init commit(create the index.html with search form)

---
+++ index.js
@@ -0,0 +1,7 @@
+const SearchForm=document.getElementById('search-form');
+const InputForm=document.getElementById('search-input');
+
+SearchForm.addEventListener('submit',e=>{
+    alert(123);
+}
+    );  
